997e371c2fea0014927a46b8bffd9322879a696d
Jessica <jessica-mss@hotmail.com>
2024-04-03 21:05:33 -0300

    Aula 2 - Exercicio proposto em aula

---
+++ aula2/Aluno.java
@@ -0,0 +1,20 @@
+package aula2;
+
+public class Aluno {
+    String nome;
+    String raca;
+    int idade;
+
+    public Aluno(String nome, String raca, int idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+
+    public void imprimirDadosAlunos() {
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Nome do raça: " + raca);
+        System.out.println("Nome do idade: " + idade);
+
+    }
+}
